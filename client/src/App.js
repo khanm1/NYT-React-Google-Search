3ee14cda6e01ceb0243c510d21ab87a433d93c40
mkhan <riponk9519@gamil.com>
2019-03-16 23:16:17 -0500

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,123 @@
-import React from 'react';
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-import MainResult from './component/MainResult';
-function App() {
-  return (
-    <MainResult />
-    // <Router>
-    //   <div>
-    //     <Route path="/" component={MainResult}/>
-    //   </div>
-    // </Router>
-  )
+import React, { Component } from "react";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import API from "./utils/API";
+import { RecipeList, RecipeListItem } from "./components/RecipeList";
+import { Container, Row, Col } from "./components/Grid";
+
+class App extends Component {
+  state = {
+    recipes: [],
+    recipeSearch: ""
+  };
+
+  handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+    event.preventDefault();
+    API.getRecipes(this.state.recipeSearch)
+      .then(res => this.setState({ recipes: res.data }))
+      .catch(err => console.log(err));
+  };
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ recipes: res.data, title: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  handleSaveBook = book=> {
+    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+   console.log(book);
+   
+    // API.saveBook({
+    //   title: this.title,
+    //   authors: this.authors,
+    //   description: this.description,
+    //   image: this.image,
+    //   link: this.link
+    // })
+    //   .then(res => console.log(res))
+    //   .catch(err => console.log(err));
+    const books= this.state.recipes.find( article => article._id === book.id);
+   API.saveBook(books).then(res => this.loadBooks);
+  
+    
+  };
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Jumbotron />
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <form>
+                <Container>
+                  <Row>
+                    <Col size="xs-9 sm-10">
+                      <Input
+                        name="recipeSearch"
+                        value={this.state.recipeSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Search For a Book"
+                      />
+                    </Col>
+                    <Col size="xs-3 sm-2">
+                      <Button
+                        onClick={this.handleFormSubmit}
+                        type="success"
+                        className="input-lg"
+                      >
+                        Search
+                      </Button>
+                    </Col>
+                  </Row>
+                </Container>
+              </form>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="xs-12">
+              {!this.state.recipes.length ? (
+                <h1 className="text-center">No Books to Display</h1>
+              ) : (
+                <RecipeList>
+                  {this.state.recipes.map(book => {
+                    return (
+                      <RecipeListItem key={book._id}
+                      book = {book}
+                      
+                      title={book.volumeInfo.title}
+                      subtitle={book.volumeInfo.subtitle}
+                      link={book.volumeInfo.infoLink}
+                      authors={book.volumeInfo.authors?book.volumeInfo.authors.join(", "):""}
+                      description={book.volumeInfo.description}
+                      image={book.volumeInfo.imageLinks ?book.volumeInfo.imageLinks.thumbnail:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png"}
+                      onClick={this.handleSaveBook}
+                      />
+                    );
+                  })}
+                </RecipeList>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
